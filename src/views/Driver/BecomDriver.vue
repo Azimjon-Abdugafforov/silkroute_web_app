7a58bfaff5a3a8e6c2d561c04976ee40e37306a9
Azimjon Abdugafforov <azimjonabdugafforov17@gmail.com>
2023-12-29 11:11:39 +0500

    adding driver section

--- src/views/Driver/BecomDriver.vue
+++ src/views/Driver/BecomDriver.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-    driver is here
+  <div class=" w-full mx-auto">
+    <div class=" w-9/12 mx-auto ">
+      <h1 class="border text-center my-4 text-2xl font-semibold text-slate-700">We are hiring professional drivers</h1>
+      <div>form</div>
+    </div>
   </div>
 </template>
 
